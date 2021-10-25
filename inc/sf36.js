50fcbd3bb0f2fcfe4051a4ed86ac214507370ec1
Alejandro Sergio D´Alessandro <90192385+uell-doc@users.noreply.github.com>
2021-10-25 14:59:36 -0300

    Update sf36.js

--- inc/sf36.js
+++ inc/sf36.js
@@ -1,8 +1,3 @@
-/*
-Sistema desenvolvido por William Castro
-Email para contato: williamccoelho@gmail.com
-*/
-
 function exe_system(){
 	var error = 0;
 
@@ -14,7 +9,7 @@ function exe_system(){
 		destacar('Q01L01');
 	}
 	else {
-		// remover destaque se tiver marcado
+		
 		if($('#L_Q01L01').is('.linha_h')) {
 			$('#L_Q01L01').removeClass('linha_h');
 			$('#L_Q01L01').addClass('even');
@@ -33,18 +28,14 @@ function exe_system(){
 		destacar('Q02L01');
 	}	
 	else {
-		// remover destaque se tiver marcado
 		if($('#L_Q02L01').is('.linha_h')) {
 			$('#L_Q02L01').removeClass('linha_h');
 			$('#L_Q02L01').addClass('even');
 		}
 	}		
+		
 	
-	
-	
-	
-	
-	// QUADRO 03 - LINHA 01
+	// QUADRO 03 - LINEA 01
 	var R_Q03L01 = 	$("input:checked[name=Q03L01]").map(function() {return $(this).val();}).get().join();
 	
 	
@@ -53,7 +44,7 @@ function exe_system(){
 		destacar('Q03L01');
 	}	
 	else {
-		// remover destaque se tiver marcado
+		
 		if($('#L_Q03L01').is('.linha_h')) {
 			$('#L_Q03L01').removeClass('linha_h');
 			$('#L_Q03L01').addClass('even');
@@ -69,14 +60,14 @@ function exe_system(){
 		destacar('Q03L02');
 	}	
 	else {
-		// remover destaque se tiver marcado
+		
 		if($('#L_Q03L02').is('.linha_h')) {
 			$('#L_Q03L02').removeClass('linha_h');
 			$('#L_Q03L02').addClass('even');
 		}
 	}	
 	
-	// QUADRO 03 - LINHA 03
+	// QUADRO 03 - LINEA 03
 	var R_Q03L03 = 	$("input:checked[name=Q03L03]").map(function() {return $(this).val();}).get().join();
 	
 	
@@ -85,7 +76,7 @@ function exe_system(){
 		destacar('Q03L03');
 	}	
 	else {
-		// remover destaque se tiver marcado
+	
 		if($('#L_Q03L03').is('.linha_h')) {
 			$('#L_Q03L03').removeClass('linha_h');
 			$('#L_Q03L03').addClass('even');
@@ -93,7 +84,7 @@ function exe_system(){
 	}		
 	
 		
-	// QUADRO 03 - LINHA 04
+	// QUADRO 03 - LINEA 04
 	var R_Q03L04 = 	$("input:checked[name=Q03L04]").map(function() {return $(this).val();}).get().join();
 	
 	
@@ -102,14 +93,14 @@ function exe_system(){
 		destacar('Q03L04');
 	}	
 	else {
-		// remover destaque se tiver marcado
+		
 		if($('#L_Q03L04').is('.linha_h')) {
 			$('#L_Q03L04').removeClass('linha_h');
 			$('#L_Q03L04').addClass('even');
 		}
 	}			
 	
-	// QUADRO 03 - LINHA 05
+	// QUADRO 03 - LINEA 05
 	var R_Q03L05 = 	$("input:checked[name=Q03L05]").map(function() {return $(this).val();}).get().join();
 	
 	
@@ -118,14 +109,14 @@ function exe_system(){
 		destacar('Q03L05');
 	}	
 	else {
-		// remover destaque se tiver marcado
+		
 		if($('#L_Q03L05').is('.linha_h')) {
 			$('#L_Q03L05').removeClass('linha_h');
 			$('#L_Q03L05').addClass('even');
 		}
 	}	
 		
-	// QUADRO 03 - LINHA 06
+	// QUADRO 03 - LINEA 06
 	var R_Q03L06 = 	$("input:checked[name=Q03L06]").map(function() {return $(this).val();}).get().join();
 	
 	
@@ -134,14 +125,14 @@ function exe_system(){
 		destacar('Q03L06');
 	}	
 	else {
-		// remover destaque se tiver marcado
+		
 		if($('#L_Q03L06').is('.linha_h')) {
 			$('#L_Q03L06').removeClass('linha_h');
 			$('#L_Q03L06').addClass('even');
 		}
 	}	
 	
-	// QUADRO 03 - LINHA 07
+	// QUADRO 03 - LINEA 07
 	var R_Q03L07 = 	$("input:checked[name=Q03L07]").map(function() {return $(this).val();}).get().join();
 	
 	
@@ -150,14 +141,14 @@ function exe_system(){
 		destacar('Q03L07');
 	}	
 	else {
-		// remover destaque se tiver marcado
+		
 		if($('#L_Q03L07').is('.linha_h')) {
 			$('#L_Q03L07').removeClass('linha_h');
 			$('#L_Q03L07').addClass('even');
 		}
 	}	
 		
-	// QUADRO 03 - LINHA 08
+	// QUADRO 03 - LINEA 08
 	var R_Q03L08 = 	$("input:checked[name=Q03L08]").map(function() {return $(this).val();}).get().join();
 	
 	
@@ -166,14 +157,14 @@ function exe_system(){
 		destacar('Q03L08');
 	}	
 	else {
-		// remover destaque se tiver marcado
+		
 		if($('#L_Q03L08').is('.linha_h')) {
 			$('#L_Q03L08').removeClass('linha_h');
 			$('#L_Q03L08').addClass('even');
 		}
 	}	
 		
-	// QUADRO 03 - LINHA 09
+	// QUADRO 03 - LINEA 09
 	var R_Q03L09 = 	$("input:checked[name=Q03L09]").map(function() {return $(this).val();}).get().join();
 	
 	
@@ -182,14 +173,14 @@ function exe_system(){
 		destacar('Q03L09');
 	}	
 	else {
-		// remover destaque se tiver marcado
+		
 		if($('#L_Q03L09').is('.linha_h')) {
 			$('#L_Q03L09').removeClass('linha_h');
 			$('#L_Q03L09').addClass('even');
 		}
 	}	
 	
-	// QUADRO 03 - LINHA 10
+	// QUADRO 03 - LINEA 10
 	var R_Q03L10 = 	$("input:checked[name=Q03L10]").map(function() {return $(this).val();}).get().join();
 	
 	
@@ -198,7 +189,7 @@ function exe_system(){
 		destacar('Q03L10');
 	}	
 	else {
-		// remover destaque se tiver marcado
+		
 		if($('#L_Q03L10').is('.linha_h')) {
 			$('#L_Q03L10').removeClass('linha_h');
 			$('#L_Q03L10').addClass('even');
@@ -209,7 +200,7 @@ function exe_system(){
 
 	
 	
-	// QUADRO 04 - LINHA 01
+	// QUADRO 04 - LINEA 01
 	var R_Q04L01 = 	$("input:checked[name=Q04L01]").map(function() {return $(this).val();}).get().join();
 	
 	if(R_Q04L01 == '') {
@@ -223,7 +214,7 @@ function exe_system(){
 		}
 	}
 	
-	// QUADRO 04 - LINHA 02
+	// QUADRO 04 - LINEA 02
 	var R_Q04L02 = 	$("input:checked[name=Q04L02]").map(function() {return $(this).val();}).get().join();
 	
 	if(R_Q04L02 == '') {
@@ -237,7 +228,7 @@ function exe_system(){
 		}
 	}
 		
-	// QUADRO 04 - LINHA 03
+	// QUADRO 04 - LINEA 03
 	var R_Q04L03 = 	$("input:checked[name=Q04L03]").map(function() {return $(this).val();}).get().join();
 	
 	if(R_Q04L03 == '') {
@@ -251,7 +242,7 @@ function exe_system(){
 		}
 	}
 	
-	// QUADRO 04 - LINHA 04
+	// QUADRO 04 - LINEA 04
 	var R_Q04L04 = 	$("input:checked[name=Q04L04]").map(function() {return $(this).val();}).get().join();
 	
 	if(R_Q04L04 == '') {
@@ -266,7 +257,7 @@ function exe_system(){
 	}			
 	
 	
-	// QUADRO 05 - LINHA 01
+	// QUADRO 05 - LINEA 01
 	var R_Q05L01 = 	$("input:checked[name=Q05L01]").map(function() {return $(this).val();}).get().join();
 	
 	if(R_Q05L01 == '') {
@@ -280,7 +271,7 @@ function exe_system(){
 		}
 	}		
 		
-	// QUADRO 05 - LINHA 02
+	// QUADRO 05 - LINEA 02
 	var R_Q05L02 = 	$("input:checked[name=Q05L02]").map(function() {return $(this).val();}).get().join();
 	
 	if(R_Q05L02 == '') {
@@ -294,7 +285,7 @@ function exe_system(){
 		}
 	}	
 	
-	// QUADRO 05 - LINHA 03
+	// QUADRO 05 - LINEA 03
 	var R_Q05L03 = 	$("input:checked[name=Q05L03]").map(function() {return $(this).val();}).get().join();
 	
 	if(R_Q05L03 == '') {
@@ -313,7 +304,7 @@ function exe_system(){
 	
 	
 	
-	// QUADRO 06 - LINHA 01
+	// QUADRO 06 - LINEA 01
 	var R_Q06L01 = 	$("input:checked[name=Q06L01]").map(function() {return $(this).val();}).get().join();
 	
 	if(R_Q06L01 == '') {
@@ -332,7 +323,7 @@ function exe_system(){
 	
 	
 
-	// QUADRO 07 - LINHA 01
+	// QUADRO 07 - LINEA 01
 	var R_Q07L01 = 	$("input:checked[name=Q07L01]").map(function() {return $(this).val();}).get().join();
 	
 	if(R_Q07L01 == '') {
@@ -347,7 +338,7 @@ function exe_system(){
 	}			
 
 	
-	// QUADRO 08 - LINHA 01
+	// QUADRO 08 - LINEA 01
 	var R_Q08L01 = 	$("input:checked[name=Q08L01]").map(function() {return $(this).val();}).get().join();
 		
 
@@ -395,7 +386,7 @@ function exe_system(){
 		
 	
 	
-	// QUADRO 09 - A LINHA 01
+	// QUADRO 09 - A LINEA 01
 	var R_Q09L01 = 	$("input:checked[name=Q09L01]").map(function() {return $(this).val();}).get().join();
 	
 	if(R_Q09L01 == '') {
@@ -403,7 +394,7 @@ function exe_system(){
 		destacar('Q09L01');
 	}	
 	else {
-		// alteração valores
+		
 		if(R_Q09L01 == 1) {
 			RF_Q09L01 = 6;
 		}
@@ -433,7 +424,7 @@ function exe_system(){
 		}
 	}	
 	
-	// QUADRO 09 - B LINHA 02
+	// QUADRO 09 - B LINEA 02
 	var R_Q09L02 = 	$("input:checked[name=Q09L02]").map(function() {return $(this).val();}).get().join();
 	
 	if(R_Q09L02 == '') {
@@ -447,7 +438,7 @@ function exe_system(){
 		}
 	}	
 	
-	// QUADRO 09 - C LINHA 03
+	// QUADRO 09 - C LINEA 03
 	var R_Q09L03 = 	$("input:checked[name=Q09L03]").map(function() {return $(this).val();}).get().join();
 	
 	if(R_Q09L03 == '') {
@@ -461,7 +452,7 @@ function exe_system(){
 		}
 	}	
 	
-	// QUADRO 09 - D LINHA 04
+	// QUADRO 09 - D LINEA 04
 	var R_Q09L04 = 	$("input:checked[name=Q09L04]").map(function() {return $(this).val();}).get().join();
 	
 	if(R_Q09L04 == '') {
@@ -469,7 +460,7 @@ function exe_system(){
 		destacar('Q09L04');
 	}		
 	else {
-		// alteração valores
+		
 		if(R_Q09L04 == 1) {
 			RF_Q09L04 = 6;
 		}
@@ -499,7 +490,7 @@ function exe_system(){
 		}
 	}	
 
-	// QUADRO 09 - E LINHA 05
+	// QUADRO 09 - E LINEA 05
 	var R_Q09L05 = 	$("input:checked[name=Q09L05]").map(function() {return $(this).val();}).get().join();
 	
 	if(R_Q09L05 == '') {
@@ -507,7 +498,7 @@ function exe_system(){
 		destacar('Q09L05');
 	}			
 	else {
-		// alteração valores
+		
 		if(R_Q09L05 == 1) {
 			RF_Q09L05 = 6;
 		}
@@ -537,7 +528,7 @@ function exe_system(){
 		}
 	}	
 
-	// QUADRO 09 - F LINHA 06
+	// QUADRO 09 - F LINEA 06
 	var R_Q09L06 = 	$("input:checked[name=Q09L06]").map(function() {return $(this).val();}).get().join();
 	
 	if(R_Q09L06 == '') {
@@ -551,7 +542,7 @@ function exe_system(){
 		}
 	}	
 	
-	// QUADRO 09 - G LINHA 07
+	// QUADRO 09 - G LINEA 07
 	var R_Q09L07 = 	$("input:checked[name=Q09L07]").map(function() {return $(this).val();}).get().join();
 	
 	if(R_Q09L07 == '') {
@@ -565,7 +556,7 @@ function exe_system(){
 		}
 	}
 	
-	// QUADRO 09 - H LINHA 08
+	// QUADRO 09 - H LINEA 08
 	var R_Q09L08 = 	$("input:checked[name=Q09L08]").map(function() {return $(this).val();}).get().join();
 	
 	if(R_Q09L08 == '') {
@@ -573,7 +564,7 @@ function exe_system(){
 		destacar('Q09L08');
 	}
 	else {
-		// alteração valores
+		
 		if(R_Q09L08 == 1) {
 			RF_Q09L08 = 6;
 		}
@@ -660,7 +651,7 @@ function exe_system(){
 		destacar('Q11L02');
 	}	
 	else {
-		// alteração valores
+		// alteraÃ§Ã£o valores
 		if(R_Q11L02 == 1) {
 			RF_Q11L02 = 5;
 		}
@@ -743,34 +734,34 @@ function exe_system(){
 	
 	
 	
-	// VERIFICAÇÃO	
+	// VERIFICAÃ‡ÃƒO	
 	if(error > 0) {
-		alert('As alternativas destacadas em cor rosa, não foram selecionadas e são obrigatórias.');
+		alert('As alternativas destacadas em cor rosa, nÃ£o foram selecionadas e sÃ£o obrigatÃ³rias.');
 		var error = 0;
 	}
 	else {
-		// SOMATÓRIA - QUADRO 01
+		// SOMATÃ“RIA - QUADRO 01
 		var R_Q01 = parseFloat(R_Q01L01);
 		
-		// SOMATÓRIA - QUADRO 02
+		// SOMATÃ“RIA - QUADRO 02
 		var R_Q02 = parseFloat(R_Q02L01);
 
-		// SOMATÓRIA - QUADRO 03
+		// SOMATÃ“RIA - QUADRO 03
 		var R_Q03 = parseFloat(R_Q03L01)+parseFloat(R_Q03L02)+parseFloat(R_Q03L03)+parseFloat(R_Q03L04)+parseFloat(R_Q03L05)+parseFloat(R_Q03L06)+parseFloat(R_Q03L07)+parseFloat(R_Q03L08)+parseFloat(R_Q03L09)+parseFloat(R_Q03L10);
 
-		// SOMATÓRIA - QUADRO 04
+		// SOMATÃ“RIA - QUADRO 04
 		var R_Q04 = parseFloat(R_Q04L01)+parseFloat(R_Q04L02)+parseFloat(R_Q04L03)+parseFloat(R_Q04L04);
 		
-		// SOMATÓRIA - QUADRO 05
+		// SOMATÃ“RIA - QUADRO 05
 		var R_Q05 = parseFloat(R_Q05L01)+parseFloat(R_Q05L02)+parseFloat(R_Q05L03);
 
-		// SOMATÓRIA - QUADRO 06
+		// SOMATÃ“RIA - QUADRO 06
 		var R_Q06 = parseFloat(R_Q06L01);	
 
-		// SOMATÓRIA - QUADRO 07
+		// SOMATÃ“RIA - QUADRO 07
 		var R_Q07 = parseFloat(R_Q07L01);	
 
-		// SOMATÓRIA - QUADRO 08
+		// SOMATÃ“RIA - QUADRO 08
 		var R_Q08 = parseFloat(RF_Q08L01);
 		
 		var R_Q09 = parseFloat(RF_Q09L01) + parseFloat(R_Q09L02) + parseFloat(R_Q09L03) + parseFloat(RF_Q09L04) + parseFloat(RF_Q09L05) + parseFloat(R_Q09L06) + parseFloat(R_Q09L07) + parseFloat(RF_Q09L08);
@@ -816,13 +807,13 @@ function exe_system(){
 					
 		$('#formulario').fadeOut('slow', function () { 
 			$('#div_capacidade').html("Capacidade funcional: "+b_capacidade+" <br /> Raw Scale: "+capacidade);
-			$('#div_limitacao').html("Limitacao por aspectos físicos: "+b_limit+" <br /> Raw Scale: "+limitacao);
+			$('#div_limitacao').html("Limitacao por aspectos fÃ­sicos: "+b_limit+" <br /> Raw Scale: "+limitacao);
 			$('#div_dor').html("Dor: "+b_dor+" <br /> Raw Scale: "+dor);
-			$('#div_estado').html("Estado geral de saúde: "+b_estado+" <br /> Raw Scale: "+estado);
+			$('#div_estado').html("Estado geral de saÃºde: "+b_estado+" <br /> Raw Scale: "+estado);
 			$('#div_vitalidade').html("Vitalidade: "+b_vitalidade+" <br /> Raw Scale: "+vitalidade);
 			$('#div_aspectos').html("Aspectos sociais: "+b_aspectos+" <br /> Raw Scale: "+aspectos);
-			$('#div_limitacao2').html("Limitação por aspectos emocionais: "+b_limitacao2 +" <br /> Raw Scale: "+limitacao2);
-			$('#div_saude').html("Saúde mental: "+b_saude +" <br /> Raw Scale: "+saude);
+			$('#div_limitacao2').html("LimitaÃ§Ã£o por aspectos emocionais: "+b_limitacao2 +" <br /> Raw Scale: "+limitacao2);
+			$('#div_saude').html("SaÃºde mental: "+b_saude +" <br /> Raw Scale: "+saude);
 			
 			$('#resultado').fadeIn('slow');
 		});
@@ -839,7 +830,7 @@ function voltar() {
 
 function resetar() {
 	$('#formulario-perguntas').each(function(){
-	        if(confirm("Você tem certeza que deseja resetar o formulário?")) {
+	        if(confirm("VocÃª tem certeza que deseja resetar o formulÃ¡rio?")) {
 		    	this.reset();
 			}
 	});
